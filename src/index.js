9f0096e8e1bddb3c3a2167aeae76efaf234f0ecf
Idan <idanfadloneng@gmail.com>
2022-03-12 18:32:44 +0200

    Firsts Files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const port =process.env.PORT || 3000;
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('<h2>Hi from server<h2>');
+});
+
+app.listen(port, ()=>{
+    console.log('server is up');
+
+});
