d41160e84629c659b03c24cfae8eb47df37d22c5
Asaf Navon <asnav1999@gmail.com>
2022-03-15 16:07:25 +0200

    installed and initialized eslint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const port =process.env.PORT || 3000;
 const app = express();
-
-app.get('/',(req,res)=>{
-    res.send('<h2>Hi from server<h2>');
+app.get('/', (req, res)=>{
+  res.send('<h2>Hi from server<h2>');
 });
-
 app.listen(port, ()=>{
-    console.log('server is up');
-
+  console.log('server is up');
 });
